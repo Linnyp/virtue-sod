cacd2fe352e528f1fddbbef5302c8073daf5392e
Linnyp <114024734+Linnyp@users.noreply.github.com>
2024-05-31 18:05:09 -0400

    contact component added props

--- src/components/component/contactform.tsx
+++ src/components/component/contactform.tsx
@@ -8,7 +8,6 @@ import { Label } from "@/components/ui/label";
 import { Input } from "@/components/ui/input";
 import { Textarea } from "@/components/ui/textarea";
 import { Button } from "@/components/ui/button";
-import Image from "../../assets/contact.png";
 
 interface contactImage {
   src?: string;
